c34c7d9310d1c8ef894082443366cc6ccb815acb
Thiago Ranniel <thiagorannialves@gmail.com>
2026-05-23 21:04:44 -0300

    feat: criação da venv

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://cewnlpxoougozkheswlr.supabase.co";
-
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNld25scHhvb3Vnb3praGVzd2xyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1NTY5NzMsImV4cCI6MjA5NTEzMjk3M30.2UHCmSjlgOv6GWOShu7Le8zJhZvSQgpnCHxekomwrlw";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
